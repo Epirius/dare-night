ae9441864b0638e33947789acb8b0ba2a8336624
Felix Kaasa <mrKaasa@protonmail.com>
2024-04-22 19:50:17 +0200

    Configured the connection to the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function HomePage() {
-  return <main className="">Main page</main>;
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  return (
+    <main className="">
+      <div>Main page</div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
+    </main>
+  );
 }
