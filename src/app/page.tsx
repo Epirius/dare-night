15230a7ba9c928a274c832234d30cab469fea55b
Felix Kaasa <mrKaasa@protonmail.com>
2024-06-03 12:36:44 +0200

    Updating the eventStatus based on the finishedAt date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
-import {Button} from "~/components/ui/button";
-import {getEventData, getMyEvents} from "~/server/queries";
-import {Card, CardDescription, CardTitle} from "~/components/ui/card"
-import {CalendarPlus, CirclePlus} from 'lucide-react';
+import { Button } from "~/components/ui/button";
+import { getEventData, getMyEvents } from "~/server/queries";
+import { Card, CardDescription, CardTitle } from "~/components/ui/card";
+import { CalendarPlus, CirclePlus } from "lucide-react";
 import EventCard from "~/components/eventCard";
-import {eventStatusEnum} from "~/utils/enum"
+import { eventStatusEnum } from "~/utils/enum";
 
 export const dynamic = "force-dynamic";
-
 
 export default function HomePage() {
   return (
@@ -26,11 +25,12 @@ export default function HomePage() {
           </p>
           <SignInButton>
             <Button>Sign In</Button>
-          </SignInButton>DiamondPlus
+          </SignInButton>
+          DiamondPlus
         </div>
       </SignedOut>
       <SignedIn>
-        <SignedInContent/>
+        <SignedInContent />
       </SignedIn>
     </main>
   );
@@ -39,37 +39,47 @@ export default function HomePage() {
 async function SignedInContent() {
   const events = await getMyEvents();
 
-    if (events instanceof Error) {
+  if (events instanceof Error) {
     return <div>Error: {events.message}</div>;
   }
-  const eventDataPromises = events.map(event => getEventData(event.id));
+  const eventDataPromises = events.map((event) => getEventData(event.id));
   const eventsData = await Promise.all(eventDataPromises);
 
   return (
     <Card>
-        <CardTitle className="pt-5 pl-5">Feel daring?</CardTitle>
-        <CardDescription className="pl-5">Here is an overview of all your dare-nights you are a part of</CardDescription>
+      <CardTitle className="pl-5 pt-5">Feel daring?</CardTitle>
+      <CardDescription className="pl-5">
+        Here is an overview of all your dare-nights you are a part of
+      </CardDescription>
       <div className="flex gap-4 p-5">
         <Link href="/new-event">
           <Button size="lg" className="px-2">
-            <CalendarPlus className="mr-2 h-4 w-4"/> Create Event
+            <CalendarPlus className="mr-2 h-4 w-4" /> Create Event
           </Button>
         </Link>
         <Link href="/join-event">
           <Button size="lg" className="px-2">
-              <CirclePlus className="mr-2 h-4 w-4"/> Join Event
+            <CirclePlus className="mr-2 h-4 w-4" /> Join Event
           </Button>
         </Link>
       </div>
-        <div className="flex justify-center items-center h-full px-4">
-            <ul className="flex flex-col items-center w-full py-5">
-                {eventsData.map((e) => (
-                    <li key={`li-${e.event.id}`} className="w-full flex justify-center mb-4 last:mb-0 py-1">
-                        <EventCard link={`/event/${e.event.id}`} name={e.event.name} members={e.member_count} eventStatus={ eventStatusEnum.Active }/>
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <div className="flex h-full items-center justify-center px-4">
+        <ul className="flex w-full flex-col items-center py-5">
+          {eventsData.map((e) => (
+            <li
+              key={`li-${e.event.id}`}
+              className="mb-4 flex w-full justify-center py-1 last:mb-0"
+            >
+              <EventCard
+                link={`/event/${e.event.id}`}
+                name={e.event.name}
+                members={e.member_count}
+                finishedAt={e.event.finishedAt}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </Card>
   );
 }
--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -1,52 +1,72 @@
 import { Card, CardDescription, CardTitle } from "~/components/ui/card";
-import { Calendar, Users } from 'lucide-react';
+import { Calendar, Users } from "lucide-react";
 import Link from "next/link";
 import { eventStatusEnum } from "src/utils/enum";
 import { Badge } from "~/components/ui/badge";
 
 interface EventCardProps {
-    name: string;
-    link: string;
-    members: number;
-    eventStatus: eventStatusEnum;
+  name: string;
+  link: string;
+  members: number;
+  finishedAt: Date;
 }
 
+const getEventStatus = (finishedAt: Date): eventStatusEnum => {
+  const now = new Date();
+  const dayBefore = new Date(finishedAt);
+  dayBefore.setDate(finishedAt.getDate() - 1);
+  if (now > finishedAt) {
+    return eventStatusEnum.Finished;
+  }
+  if (now < dayBefore) {
+    return eventStatusEnum.Upcoming;
+  }
+  return eventStatusEnum.Active;
+};
+
 const EventCard = (props: EventCardProps) => {
-    return (
-        <Card className="flex w-full pb-5 relative">
-            <Link href={props.link} className="w-full">
-                <CardTitle className="pl-5 pt-5 relative">
-                    {props.name}
-                    <Badge variant="secondary" className={`
-                        absolute right-5 top-1/2 transform -translate-y-1/2
+  const eventStatus = getEventStatus(props.finishedAt);
+  return (
+    <Card className="relative flex w-full pb-5">
+      <Link href={props.link} className="w-full">
+        <CardTitle className="relative pl-5 pt-5">
+          {props.name}
+          <Badge
+            variant="secondary"
+            className={`
+                        absolute right-5 top-1/2 -translate-y-1/2 transform
                         ${
-                        props.eventStatus === eventStatusEnum.Active ? 'bg-green-600' :
-                            props.eventStatus === eventStatusEnum.Finished ? 'bg-red-600' :
-                                props.eventStatus === eventStatusEnum.Upcoming? 'bg-yellow-400' :
-                                    ''
-                    }`}>{props.eventStatus}</Badge>
-                </CardTitle>
-                <div className="flex row space-x-10 pl-7 pt-2">
-                    <div className="flex row">
-                        <CardDescription>
-                            <Users />
-                        </CardDescription>
-                        <CardDescription className="text-xl pl-2">
-                            {props.members}
-                        </CardDescription>
-                    </div>
-                    <div className="flex row">
-                        <CardDescription>
-                            <Calendar />
-                        </CardDescription>
-                        <CardDescription className="text-xl pl-2">
-                            10-12-24
-                        </CardDescription>
-                    </div>
-                </div>
-            </Link>
-        </Card>
-    );
-}
+                          eventStatus === eventStatusEnum.Active
+                            ? "bg-green-600"
+                            : eventStatus === eventStatusEnum.Finished
+                              ? "bg-red-600"
+                              : eventStatus === eventStatusEnum.Upcoming
+                                ? "bg-yellow-400"
+                                : ""
+                        }`}
+          >
+            {eventStatus}
+          </Badge>
+        </CardTitle>
+        <div className="row flex space-x-10 pl-7 pt-2">
+          <div className="row flex">
+            <CardDescription>
+              <Users />
+            </CardDescription>
+            <CardDescription className="pl-2 text-xl">
+              {props.members}
+            </CardDescription>
+          </div>
+          <div className="row flex">
+            <CardDescription>
+              <Calendar />
+            </CardDescription>
+            <CardDescription className="pl-2 text-xl">10-12-24</CardDescription>
+          </div>
+        </div>
+      </Link>
+    </Card>
+  );
+};
 
 export default EventCard;
