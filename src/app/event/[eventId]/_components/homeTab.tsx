c012bbfbccdeb10567e07d8fd54b36b8566ff65f
Felix Kaasa <mrKaasa@protonmail.com>
2024-06-04 13:56:07 +0200

    tasks are now showed inside their category accordion

--- src/app/event/[eventId]/_components/homeTab.tsx
+++ src/app/event/[eventId]/_components/homeTab.tsx
@@ -7,6 +7,12 @@ import {
 import { TaskCard } from "./taskCard";
 import { useEffect, useState } from "react";
 import { pusherClient } from "~/lib/pusherClient";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
 
 type HomeTabProps = {
   taskData: Awaited<ReturnType<typeof getTasks>>;
@@ -23,6 +29,7 @@ export default function HomeTab({
   taskData,
   userTeamId,
   eventId,
+  categories,
 }: HomeTabProps) {
   const [data, setData] = useState(taskData);
 
@@ -58,32 +65,78 @@ export default function HomeTab({
         <h3 className="pb-4 pt-6 text-2xl font-semibold">Tasks:</h3>
       )}
       {data.length === 0 && <p>No tasks yet</p>}
-      {data
-        .filter((task) => !task.completionData?.completed ?? true)
-        .map((task) => {
-          return (
-            <TaskCard
-              key={`task-card-${task.id}`}
-              data={task}
-              teamId={userTeamId}
-            />
-          );
-        })}
-      {data[0]?.completionData &&
-        data.find((task) => task.completionData?.completed) && (
-          <h3 className="pb-4 pt-6 text-2xl font-semibold">completed tasks:</h3>
-        )}
-      {data
-        .filter((task) => task.completionData?.completed ?? false)
-        .map((task) => {
-          return (
-            <TaskCard
-              key={`task-card-${task.id}`}
-              data={task}
-              teamId={userTeamId}
-            />
-          );
-        })}
+      {categories.map((category) => {
+        return (
+          <CategoryAccordion
+            key={`category-accordion-${category.id}-${category.name}`}
+            data={data.filter((task) => task.category === category.id)}
+            category={category}
+            userTeamId={userTeamId}
+          />
+        );
+      })}
+      <CategoryAccordion
+        data={data.filter((task) => !task.category)}
+        category={undefined}
+        userTeamId={userTeamId}
+      />
+    </div>
+  );
+}
+
+type CategoryAccordionProps = {
+  data: Awaited<ReturnType<typeof getTasks>>;
+  category?: Awaited<ReturnType<typeof getCategory>>[number];
+  userTeamId: Awaited<ReturnType<typeof getUserTeamId>>;
+};
+
+function CategoryAccordion({
+  data,
+  category,
+  userTeamId,
+}: CategoryAccordionProps) {
+  if (data.length === 0) {
+    return null;
+  }
+  return (
+    <div>
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value={category?.name ?? "no-category"}>
+          <AccordionTrigger>
+            <h3>{category?.name ?? "No category"}</h3>
+          </AccordionTrigger>
+          <AccordionContent>
+            {data
+              .filter((task) => !task.completionData?.completed ?? true)
+              .map((task) => {
+                return (
+                  <TaskCard
+                    key={`task-card-${task.id}`}
+                    data={task}
+                    teamId={userTeamId}
+                  />
+                );
+              })}
+            {data[0]?.completionData &&
+              data.find((task) => task.completionData?.completed) && (
+                <h3 className="pb-4 pt-6 text-2xl font-semibold">
+                  completed tasks:
+                </h3>
+              )}
+            {data
+              .filter((task) => task.completionData?.completed ?? false)
+              .map((task) => {
+                return (
+                  <TaskCard
+                    key={`task-card-${task.id}`}
+                    data={task}
+                    teamId={userTeamId}
+                  />
+                );
+              })}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
